d34b4c22515cdccad4908036fe1df60d8215752b
Pepijn de Vos <pepijndevos@gmail.com>
2018-04-24 13:48:35 +0200

    dac.py

--- transfer.py
+++ transfer.py
@@ -1,18 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
-from math import sqrt
+from math import sqrt, pi
 
-R = 16
-C = 1e-6
-L = 680e-6
+R = 4
+C = 0.332e-6
+L = 21.3e-6
 
-D = 0.5
+#actually D'
+Dmin = 1/3
+Dmax = 1
 
 def ctl2out(R, L, C, D):
+    w0 = D/sqrt(L*C)
+    wz = D**2*R/L
+    Q = D*R*sqrt(C/L)
+    print(w0/2/pi, wz/2/pi, Q)
     return signal.TransferFunction(
-        [-L/(R*D**2), 1],
-        [L*C/(D**2), L/(R*D**2), 1])
+        [-1/wz, 1],
+        #[1],
+        [1/w0**2, 1/(w0*Q), 1])
 
 def predistort(R, L, C, D):
     return signal.TransferFunction(
@@ -23,21 +30,44 @@ def predistort(R, L, C, D):
 def bodeplots(R, L, C, num):
     plt.figure()
     ax1 = plt.subplot(211)
+    ax1.grid(which='both')
     ax2 = plt.subplot(212)
-    for D in np.linspace(0.1, 0.9, num):
+    ax2.grid(which='both')
+    for D in np.linspace(Dmin, Dmax, num):
         sys = ctl2out(R, L, C, D)
 
         wb, mag, phase = sys.bode()
 
-        ax1.semilogx(wb, mag, label="D=%.2f"%D)    # Bode magnitude plot
-        ax2.semilogx(wb, phase, label="D=%.2f"%D)  # Bode phase plot
+        ax1.semilogx(wb/2/pi, mag, label="D=%.2f"%D)    # Bode magnitude plot
+        ax2.semilogx(wb/2/pi, phase, label="D=%.2f"%D)  # Bode phase plot
 
     ax1.legend()
     ax2.legend()
 
+def polebode(R, L, C, num):
+    plt.figure()
+    plt.grid(which='both')
+    for D in np.linspace(Dmin, Dmax, num):
+        sys = ctl2out(R, L, C, D)
+
+        pz = signal.TransferFunction([1], [-1/sys.poles[0], 1])
+        wb, mag, phase = pz.bode()
+        plt.semilogx(wb/2/pi, mag, label="P0 D=%.2f"%D)    # Bode magnitude plot
+
+        pz = signal.TransferFunction([1], [-1/sys.poles[1], 1])
+        wb, mag, phase = pz.bode()
+        plt.semilogx(wb/2/pi, mag, label="P1 D=%.2f"%D)    # Bode magnitude plot
+
+        pz  = signal.TransferFunction([-1/sys.zeros[0], 1], [1])
+        wb, mag, phase = pz.bode()
+        plt.semilogx(wb/2/pi, mag, label="Z D=%.2f"%D)    # Bode magnitude plot
+
+    plt.legend()
+
 def step(R, L, C, num):
     plt.figure()
-    for D in np.linspace(0.1, 0.9, num):
+    plt.grid(which='both')
+    for D in np.linspace(Dmin, Dmax, num):
         sys = ctl2out(R, L, C, D)
 
         t, yout = sys.step()
@@ -48,20 +78,22 @@ def step(R, L, C, num):
 
 def poles(R, L, C, num):
     plt.figure()
+    plt.grid(which='both')
     p1 = []
     p2 = []
     z = []
-    for D in np.linspace(0.1, 0.9, num):
+    for D in np.linspace(Dmin, Dmax, num):
         sys = ctl2out(R, L, C, D)
         z.append(sys.zeros[0])
         p1.append(sys.poles[0])
         p2.append(sys.poles[1])
 
-    plt.plot(np.real(p1), np.imag(p1), "-X")
-    plt.plot(np.real(p2), np.imag(p2), "-X")
-    plt.plot(np.real(z), np.imag(z), "-o")
+    plt.plot(np.real(p1)/2/pi, np.imag(p1)/2/pi, "-o")
+    plt.plot(np.real(p2)/2/pi, np.imag(p2)/2/pi, "-o")
+    plt.plot(np.real(z)/2/pi, np.imag(z)/2/pi, "-o")
 
 if __name__ == "__main__":
+    polebode(R, L, C, 2)
     bodeplots(R, L, C, 5)
     step(R, L, C, 8)
     poles(R, L, C, 15)
