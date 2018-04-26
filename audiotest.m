11ba92f038c833bf8d95cbcda5c477d504dab730
Pepijn de Vos <pepijndevos@gmail.com>
2018-04-26 20:29:26 +0200

    sine input test

--- audiotest.m
+++ audiotest.m
@@ -2,20 +2,28 @@ R = 4;
 C = 1e-6;
 L = 64*C;
 
-[y, Fs] = audioread('sultans.wav', [44100, 2*44100]);
+if 0
+    [y, Fs] = audioread('sultans.wav', [44100, 2*44100]);
+    ym=y(:,1); % mono
+else
+    Fs = 44100;
+    t = linspace(0,0.1,4410);
+    f = 1000;
+    ym = sin(2*pi*f*t);
+end
+
 Ts=1/Fs;
-dur=length(y)*Ts;
-t=linspace(0,dur,length(y));
-ym=y(:,1); % mono
+dur=length(ym)*Ts;
+t=linspace(0,dur,length(ym));
 
 yp1=(ym+1)/2;
-%ys1=1./(yp1*2/3+1/3)/3;
-ys1=yp1*2/3+1/3;
+ys1=1./(yp1*2/3+1/3)/3;
+%ys1=yp1*2/3+1/3;
 D1=timeseries(ys1*2-1, t);
 
 yp2=(-ym+1)/2;
-%ys2=1./(yp2*2/3+1/3)/3;
-ys2=yp2*2/3+1/3;
+ys2=1./(yp2*2/3+1/3)/3;
+%ys2=yp2*2/3+1/3;
 D2=timeseries(ys2*2-1, t);
 
 %%
