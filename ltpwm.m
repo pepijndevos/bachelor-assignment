5902b18c74a8d23b29fa39dc98793abeaf456800
Pepijn de Vos <pepijndevos@gmail.com>
2018-05-24 15:53:17 +0200

    dac.py

--- ltpwm.m
+++ ltpwm.m
@@ -29,7 +29,7 @@ pwm2  = interp1(t, pwm,t2);
 
 TT = array2timetable(pwm2,'RowTimes',seconds(t2));
 
-nt = seconds(linspace(t(1), t(end)));
+nt = seconds(linspace(t(1), t(end), 500));
 TT2 = retime(TT,nt,'mean');
 
 %%
@@ -37,25 +37,37 @@ close all;
 
 figure()
 ax1 = subplot(2,1,1);
-area(TT2.Time, TT2(:,[gnd1,loop1,fwd1]).Variables)
+area(TT2.Time, TT2(:,[gnd1,loop1,fwd1]).Variables*20)
 legend('V(gnd1)','V(loop1)','V(fwd)')
+ylabel('duty cycle (%)')
+ylim([0,100])
+xlabel('time')
 
 ax2 = subplot(2,1,2);
-area(TT2.Time, TT2(:,[gnd2,loop2,fwd2]).Variables)
+area(TT2.Time, TT2(:,[gnd2,loop2,fwd2]).Variables*20)
 legend('V(gnd2)','V(loop2)','V(fwd2)')
+ylabel('duty cycle (%)')
+ylim([0,100])
+xlabel('time')
 
 figure()
 ax3 = subplot(2,1,1);
 area(TT2.Time, TT2(:,[isw3, isw2, isw1]).Variables)
 legend('I(fwd1)','I(loop1)','I(gnd1)')
+ylabel('current (A)')
+xlabel('time')
 
 ax4 = subplot(2,1,2);
 area(TT2.Time, TT2(:,[isw5, isw6, isw4]).Variables)
 legend('I(fwd2)','I(loop2)','I(gnd2)')
+ylabel('current (A)')
+xlabel('time')
 
 figure()
 ax5 = axes();
 plot(TT2.Time, TT2(:,vout1).Variables-TT2(:,vout2).Variables)
 legend('V(out)')
+ylabel('voltage (V)')
+xlabel('time')
 
 linkaxes([ax1,ax2,ax3,ax4, ax5],'x')
